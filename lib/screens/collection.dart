48e8b0eb4667c30135cc44799e724cd95ff9907d
TayyabAsghar <muhammadtayyabasghar@gmail.com>
2021-04-29 02:33:44 +0500

    💄 IMPROVE-UI: Add padding to the list view

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -17,39 +17,37 @@ class _CollectionState extends State<Collection> {
       drawer: Drawer(
         child: NavDrawer(),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            SimpleCard(
-              imageUrl: 'https://source.unsplash.com/200x200/?fire',
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 5.0),
+        children: [
+          SimpleCard(
+            imageUrl: 'https://source.unsplash.com/200x200/?fire',
+            description:
+                'This is a random picture from the Unsplash.com with the tag of \'Fire\'',
+            tag: 'Fire',
+            tagIcon: Icons.whatshot_outlined,
+          ),
+          SimpleCard(
+            imageUrl: 'https://source.unsplash.com/200x200/?water',
+            description:
+                'This is a random picture from the Unsplash.com with the tag of \'Water\'',
+            tag: 'Water',
+            tagIcon: Icons.opacity,
+          ),
+          SimpleCard(
+            imageUrl: 'https://source.unsplash.com/200x200/?snow',
+            description:
+                'This is a random picture from the Unsplash.com with the tag of \'Snow\'',
+            tag: 'Snow',
+            tagIcon: Icons.ac_unit_rounded,
+          ),
+          SimpleCard(
+              imageUrl: 'https://source.unsplash.com/200x200/?lightning',
               description:
-                  'This is a random picture from the Unsplash.com with the tag of \'Fire\'',
-              tag: 'Fire',
-              tagIcon: Icons.whatshot_outlined,
-            ),
-            SimpleCard(
-              imageUrl: 'https://source.unsplash.com/200x200/?water',
-              description:
-                  'This is a random picture from the Unsplash.com with the tag of \'Water\'',
-              tag: 'Water',
-              tagIcon: Icons.opacity,
-            ),
-            SimpleCard(
-              imageUrl: 'https://source.unsplash.com/200x200/?snow',
-              description:
-                  'This is a random picture from the Unsplash.com with the tag of \'Snow\'',
-              tag: 'Snow',
-              tagIcon: Icons.ac_unit_rounded,
-            ),
-            SimpleCard(
-                imageUrl: 'https://source.unsplash.com/200x200/?lightning',
-                description:
-                    'This is a random picture from the Unsplash.com with the tag of \'Lightning\'',
-                tag: 'Lightning',
-                tagIcon: Icons.flash_on_outlined),
-          ],
-        ),
+                  'This is a random picture from the Unsplash.com with the tag of \'Lightning\'',
+              tag: 'Lightning',
+              tagIcon: Icons.flash_on_outlined),
+        ],
       ),
     );
   }
