d4efae0200c4895a3736566607eceb85dc33e54f
TayyabAsghar <muhammadtayyabasghar@gmail.com>
2021-05-04 09:38:52 +0500

    ✨ NEW-FEATURE: Add expantion tile in the Drawer.

--- lib/components/navDrawer.dart
+++ lib/components/navDrawer.dart
@@ -1,6 +1,77 @@
 import 'package:flutter/material.dart';
 import 'customAlertDialog.dart';
 
+class Entry {
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+  final String title;
+  final List<Entry> children;
+}
+
+// The entire multilevel list displayed by this app.
+final List<Entry> data = <Entry>[
+  Entry(
+    'Chapter A',
+    <Entry>[
+      Entry(
+        'Section A0',
+        <Entry>[
+          Entry('Item A0.1'),
+          Entry('Item A0.2'),
+          Entry('Item A0.3'),
+        ],
+      ),
+      Entry('Section A1'),
+      Entry('Section A2'),
+    ],
+  ),
+  Entry(
+    'Chapter B',
+    <Entry>[
+      Entry('Section B0'),
+      Entry('Section B1'),
+    ],
+  ),
+  Entry(
+    'Chapter C',
+    <Entry>[
+      Entry('Section C0'),
+      Entry('Section C1'),
+      Entry(
+        'Section C2',
+        <Entry>[
+          Entry('Item C2.0'),
+          Entry('Item C2.1'),
+          Entry('Item C2.2'),
+          Entry('Item C2.3'),
+        ],
+      ),
+    ],
+  ),
+];
+
+// Displays one Entry. If the entry has children then it's displayed
+// with an ExpansionTile.
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
+  }
+}
+
 class NavDrawer extends StatelessWidget {
   final _fontSize = 16.0;
 
@@ -110,19 +181,50 @@ class NavDrawer extends StatelessWidget {
               Navigator.pushNamed(context, '/transaction');
           },
         ),
-        ListTile(
-          enabled: false,
-          leading: Icon(Icons.schedule),
+        ExpansionTile(
+          leading: Icon(Icons.storage),
           title: Text(
-            'Recent',
+            'Records',
             style: TextStyle(
               fontSize: _fontSize,
               color: _currentRoute == '' ? Colors.blue : Colors.grey[600],
             ),
           ),
-          selectedTileColor: Colors.black.withOpacity(.5),
-          selected: _currentRoute == '',
-          onTap: () {},
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.only(
+                left: 32,
+                right: 32,
+              ),
+              leading: Icon(Icons.visibility_sharp),
+              title: Text(
+                'View Records',
+                style: TextStyle(
+                  fontSize: _fontSize,
+                  color: _currentRoute == '' ? Colors.blue : Colors.grey[600],
+                ),
+              ),
+              selectedTileColor: Colors.black.withOpacity(.5),
+              selected: _currentRoute == '',
+              onTap: () {},
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.only(
+                left: 32,
+              ),
+              leading: Icon(Icons.add),
+              title: Text(
+                'Add Records',
+                style: TextStyle(
+                  fontSize: _fontSize,
+                  color: _currentRoute == '' ? Colors.blue : Colors.grey[600],
+                ),
+              ),
+              selectedTileColor: Colors.black.withOpacity(.5),
+              selected: _currentRoute == '',
+              onTap: () {},
+            )
+          ],
         ),
         ListTile(
           enabled: false,
