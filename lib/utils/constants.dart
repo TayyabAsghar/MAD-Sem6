f98a0d41f30dc4ef21585a6b7f7e219525ec6b25
TayyabAsghar <muhammadtayyabasghar@gmail.com>
2021-05-06 01:29:27 +0500

    💡 COMMENTS: About the the urls used in the project.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,3 @@
+// Both the urls are temporary and may not work after sometime.
 final fetchApiURI = Uri.parse('https://pcc.edu.pk/ws/list/ss_consumers.php');
 final createApiURI = Uri.parse('https://pcc.edu.pk/ws/create/ss_consumers.php');
