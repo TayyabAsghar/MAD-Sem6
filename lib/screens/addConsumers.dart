257974d7a150066c54778637989e9661d15addeb
TayyabAsghar <muhammadtayyabasghar@gmail.com>
2021-05-05 22:48:35 +0500

    💄 IMPROVE-UI: Improve the look of the add consumers screen.

--- lib/screens/addConsumers.dart
+++ lib/screens/addConsumers.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import '../utils/constants.dart';
 import '../components/navDrawer.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../components/customAlertDialog.dart';
@@ -91,92 +92,121 @@ class _AddConsumersState extends State<AddConsumers> {
       drawer: Drawer(
         child: NavDrawer(),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
+      body: Container(
+        child: SingleChildScrollView(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Hero(
-                tag: 'logo',
-                child: Image.asset(
-                  'assets/images/Customer.jpg',
-                  width: 150,
+              Padding(
+                padding: EdgeInsets.only(top: 18.0, bottom: 10.0),
+                child: Hero(
+                  tag: 'logo',
+                  child: Image.asset(
+                    'assets/images/Customer.jpg',
+                    width: 150,
+                  ),
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Flexible(
-                    child: TextField(
-                      style: TextStyle(fontSize: 20),
-                      controller: fNameController,
-                      decoration: InputDecoration(labelText: 'First Name'),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20.0, 6.0, 12.0, 6.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 20),
+                        controller: fNameController,
+                        decoration: InputDecoration(labelText: 'First Name'),
+                      ),
                     ),
                   ),
                   Flexible(
-                    child: TextField(
-                      style: TextStyle(fontSize: 20),
-                      controller: lNameController,
-                      decoration: InputDecoration(labelText: 'Last Name'),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Level:',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  Flexible(
-                    child: DropdownButton<String>(
-                      value: genderController,
-                      isExpanded: true,
-                      items: ['Male', 'Female']
-                          .map(
-                            (val) => DropdownMenuItem(
-                              child: Text(
-                                val,
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              value: val,
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (String value) =>
-                          setState(() => genderController = value),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.teal,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(12.0, 6.0, 20.0, 6.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 20),
+                        controller: lNameController,
+                        decoration: InputDecoration(labelText: 'Last Name'),
                       ),
                     ),
                   ),
                 ],
               ),
-              Flexible(
-                child: TextField(
-                  style: TextStyle(fontSize: 20),
-                  controller: emailController,
-                  decoration: InputDecoration(labelText: 'Email'),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 18.0, 20.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Gender',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Flexible(
+                      child: DropdownButton<String>(
+                        value: genderController,
+                        isExpanded: true,
+                        items: ['Male', 'Female']
+                            .map(
+                              (val) => DropdownMenuItem(
+                                child: Text(
+                                  val,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                value: val,
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (String value) =>
+                            setState(() => genderController = value),
+                        underline: Container(
+                          height: 2,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Flexible(
-                child: TextField(
-                  style: TextStyle(fontSize: 20),
-                  controller: phoneController,
-                  decoration: InputDecoration(labelText: 'Phone'),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 4.0,
+                  ),
+                  child: TextField(
+                    style: TextStyle(fontSize: 20),
+                    controller: emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 6.0,
+                  ),
+                  child: TextField(
+                    style: TextStyle(fontSize: 20),
+                    controller: phoneController,
+                    decoration: InputDecoration(labelText: 'Phone Number'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(15),
+                padding: EdgeInsets.symmetric(vertical: 32.0),
                 child: ElevatedButton(
                   onPressed: sendConsumersData,
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: EdgeInsets.all(8.0),
                     child: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
